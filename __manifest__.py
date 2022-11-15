8c6c38b7213c13a321c93615e1510c552d7ccd8e
SXE-2021a <blackhorsef8@gmail.com>
2022-11-15 12:34:42 +0100

    15Nov22

--- __manifest__.py
+++ __manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         'views/informacion.xml',
+        'views/suceso.xml',
         'views/templates.xml',
         'views/menu.xml',
         'security/ir.model.access.csv',
--- models/informacion.py
+++ models/informacion.py
@@ -11,8 +11,21 @@ class informacion(models.Model):
     peso = fields.Float(string="Peso en KGs.",default=4.3,digits=(6,2))
     sexo_traducido = fields.Selection([('Mujer','Muller'),('Hombre','Home'),('Otros','Outros')],string="Sexo")
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
-    alto_en_cms = fields.Integer(string="Alto en Centímetros:")
-    ancho_en_cms = fields.Integer(string="Ancho en Centímetros:")
-    longo_en_cms = fields.Integer(string="Longo en Centímetros:")
+    alto_en_cms = fields.Integer(string="Alto en Cms:")
+    ancho_en_cms = fields.Integer(string="Ancho en Cms:")
+    longo_en_cms = fields.Integer(string="Longo en Cms:")
+    volume = fields.Float(compute="_volume",string="Volume en Metros Cúbicos",digits=(6,6), store=True)
+    densidade = fields.Float(compute="_densidade",string="Densidade en Kgs/Metros Cúbicos", store=True)
 
-
+    @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
+    def _volume(self):
+      for rexistro in self:
+          rexistro.volume =  (float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) *
+                              float(rexistro.ancho_en_cms))/1000000
+    @api.depends('peso', 'volume')
+    def _densidade(self):
+        for rexistro in self:
+            if rexistro.volume != 0:
+                rexistro.densidade = (float(rexistro.peso) / float(rexistro.volume))
+            else:
+                rexistro.densidade = 0
